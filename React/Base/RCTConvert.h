146bfbcf1d19701baf447cd6041ce555f711e6b5
Daniel Lahyani <dlahyani@lightricks.com>
2016-10-24 15:19:57 +0300

    RCTConvert: Add casting to the enum type.

--- React/Base/RCTConvert.h
+++ React/Base/RCTConvert.h
@@ -210,7 +210,7 @@ RCT_CUSTOM_CONVERTER(type, type, [RCT_DEBUG ? [self NSNumber:json] : json getter
   dispatch_once(&onceToken, ^{                            \
     mapping = values;                                     \
   });                                                     \
-  return [RCTConvertEnumValue(#type, mapping, @(default), json) getter]; \
+  return (type)[RCTConvertEnumValue(#type, mapping, @(default), json) getter]; \
 }
 
 /**
@@ -225,7 +225,7 @@ RCT_CUSTOM_CONVERTER(type, type, [RCT_DEBUG ? [self NSNumber:json] : json getter
   dispatch_once(&onceToken, ^{                            \
     mapping = values;                                     \
   });                                                     \
-  return [RCTConvertMultiEnumValue(#type, mapping, @(default), json) getter]; \
+  return (type)[RCTConvertMultiEnumValue(#type, mapping, @(default), json) getter]; \
 }
 
 /**
